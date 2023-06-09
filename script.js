9bcbeabcce778c99534dfe7ad1f3376867e8904f
sirius-08 <kalra.tanish8@gmail.com>
2023-06-09 17:13:18 +0530

    Changed the way playRound computes the winner
    In Rock Paper Scissors
    If the (clockwise cyclic)distance between userChoice & ComputerChoice is:
        0 - It's a tie
        1 - User Looses
        2 - User Wins

--- script.js
+++ script.js
@@ -7,3 +7,39 @@ function getComputerChoice() {
     else
         return "SCISSORS"; 
 }
+
+function stringChoiceToNumericChoice(choice) {
+    choice = choice.toUpperCase();
+    if(choice == "ROCK") {
+        return 0;
+    }
+    else if(choice == "PAPER") {
+        return 1;
+    }
+    else {
+        return 2;
+    }
+}
+
+function capitalize(str) {
+    return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
+}
+
+function playRound(playerSelection, computerSelection) {
+    let playerSelectionNumeric = stringChoiceToNumericChoice(playerSelection)
+    let computerSelectionNumeric = stringChoiceToNumericChoice(computerSelection);
+
+    let diff = playerSelectionNumeric - computerSelectionNumeric + 3;
+    console.log(diff);
+    
+    switch(diff%3) {
+        case 0:
+            return "It's a Tie";
+        case 1:
+            return `You Win! ${capitalize(playerSelection)} beats ${capitalize(computerSelection)}`;
+        case 2:
+            return `You Lose! ${capitalize(computerSelection)} beats ${capitalize(playerSelection)}`;
+    }
+
+    return "It's a tie";
+}
